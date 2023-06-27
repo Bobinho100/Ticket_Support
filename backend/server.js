34c027caf4fe2e2a05885b3da8d7910f74e35f5c
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-06-27 11:22:53 -0600

    Created routes for user register and login

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log('Server')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+
+const PORT = process.env.PORT || 8000
+
+
+
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Welcome to the support API"})
+
+})
+// Routes
+app.use('/api/users', require('./routes/userRoutes') )
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
