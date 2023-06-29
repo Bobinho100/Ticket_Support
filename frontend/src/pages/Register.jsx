0e429eb0e45a4a41c8361cbb1ceec8b92c1ae787
Bobinho100 <114113147+Bobinho100@users.noreply.github.com>
2023-06-28 23:24:09 -0600

    created  Home, register, login pages

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+import { FaUser } from "react-icons/fa"
+
+const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: '',
+    })
+
+    const {name, email, password, password2} = formData
+  return (
+    <>
+        <section className="heading">
+            <h1>
+
+
+            </h1>
+
+
+        </section>
+
+
+
+    </>
+  )
+}
+
+export default Register
